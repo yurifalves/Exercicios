64263d31607cd24dea2f76341a7034c9c158e727
Yuri Fuchida Alves <yurifuchidaalves@gmail.com>
2021-10-20 04:56:46 -0300

    Ajustes nas Pastas
    ajustes 1002 e 1003

---
+++ URI/Python/URI1002.py
@@ -0,0 +1,8 @@
+'''
+URI #1002 - Área do Círculo
+Python 3.9
+'''
+pi = 3.14159
+raio = float(input())
+area = pi*raio**2
+print(f'A={area:.4f}')
---
+++ URI/Python/URI1003.py
@@ -0,0 +1,8 @@
+'''
+URI #1003 - Soma Simples
+Python 3.9
+'''
+A = int(input())
+B = int(input())
+SOMA = A + B
+print(f'SOMA = {SOMA}')
