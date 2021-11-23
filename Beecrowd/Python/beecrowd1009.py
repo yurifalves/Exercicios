f948296f63a201a9ef03f64e4e1853d4b5286266
Yuri Fuchida Alves <yurifuchidaalves@gmail.com>
2021-11-23 20:05:46 -0300

    att

---
+++ Beecrowd/Python/beecrowd1009.py
@@ -0,0 +1,7 @@
+# EX 1009 - Salário com Bônus
+# Python 3.9
+
+num = int(input())
+horas = int(input())
+valor_hora = float(input())
+print(f'NUMBER = {num}\nSALARY = U$ {(horas*valor_hora):.2f}')
---
+++ Beecrowd/Python/beecrowd1010.py
@@ -0,0 +1,7 @@
+# EX 1010 - Cálculo Simples
+# Python 3.9
+
+num = int(input())
+horas = int(input())
+valor_hora = float(input())
+print(f'NUMBER = {num}\nSALARY = U$ {(horas*valor_hora):.2f}')
