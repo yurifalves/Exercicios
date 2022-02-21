8432539dd948226a0498699cc99ab51eaf947940
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-02-21 03:30:36 -0300

    create ex001, ex002

---
+++ CEV/C/ex002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("<<< EX002 - Especiais >>>\n\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("\\a\t= \tBeep\n");
+    printf("\\n\t= \tNova linha\n");
+    printf("\\t\t= \tTabulação\n");
+    printf("\\\\\t= \tContrabarra\n");
+    printf("%%%%\t= \tPorcentagem\n");
+    printf("\\\?\t= \tInterrogação\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+}
