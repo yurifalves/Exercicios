578c28eda3697a519828fcfefbea66c10ba46f18
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-03-31 15:13:46 -0300

    botatt-requisicaoapi

--- Outros Exercícios/Python/botcotacoes.py
+++ Outros Exercícios/Python/botcotacoes.py
@@ -1,9 +1,42 @@
 import telebot
 import pandas_datareader as web
+import requests
 import matplotlib.pyplot as plt
 import os
 
 bot = telebot.TeleBot(TOKEN)
+
+
+moedas_dic = {'USD-BRL': 'Dólar Americano/Real Brasileiro', 'EUR-BRL': 'Euro/Real Brasileiro',
+              'GBP-BRL': 'Libra Esterlina/Real Brasileiro',
+              'AUD-BRL': 'Dólar Australiano/Real Brasileiro',
+              'CAD-BRL': 'Dólar Canadense/Real Brasileiro', 'NZD-BRL': 'Dólar Neozelandês/Real Brasileiro',
+              'HKD-BRL': 'Dólar de Hong Kong/Real Brasileiro', 'SGD-BRL': 'Dólar de Cingapura/Real Brasileiro',
+              'TWD-BRL': 'Dólar Taiuanês/Real Brasileiro', 'ARS-BRL': 'Peso Argentino/Real Brasileiro',
+              'BOB-BRL': 'Boliviano/Real Brasileiro',
+              'AED-BRL': 'Dirham dos Emirados/Real Brasileiro',
+              'BTC-BRL': 'Bitcoin/Real Brasileiro',
+              'CHF-BRL': 'Franco Suíço/Real Brasileiro', 'CLP-BRL': 'Peso Chileno/Real Brasileiro',
+              'CNY-BRL': 'Yuan Chinês/Real Brasileiro', 'COP-BRL': 'Peso Colombiano/Real Brasileiro',
+              'DKK-BRL': 'Coroa Dinamarquesa/Real Brasileiro', 'DOGE-BRL': 'Dogecoin/Real Brasileiro',
+              'ETH-BRL': 'Ethereum/Real Brasileiro',
+              'ILS-BRL': 'Novo Shekel Israelense/Real Brasileiro',
+              'INR-BRL': 'Rúpia Indiana/Real Brasileiro',
+              'JPY-BRL': 'Iene Japonês/Real Brasileiro',
+              'LTC-BRL': 'Litecoin/Real Brasileiro',
+              'MXN-BRL': 'Peso Mexicano/Real Brasileiro',
+              'NOK-BRL': 'Coroa Norueguesa/Real Brasileiro',
+              'PEN-BRL': 'Sol do Peru/Real Brasileiro',
+              'PLN-BRL': 'Zlóti Polonês/Real Brasileiro',
+              'PYG-BRL': 'Guarani Paraguaio/Real Brasileiro',
+              'RUB-BRL': 'Rublo Russo/Real Brasileiro',
+              'SAR-BRL': 'Riyal Saudita/Real Brasileiro',
+              'SEK-BRL': 'Coroa Sueca/Real Brasileiro',
+              'THB-BRL': 'Baht Tailandês/Real Brasileiro',
+              'TRY-BRL': 'Nova Lira Turca/Real Brasileiro',
+              'UYU-BRL': 'Peso Uruguaio/Real Brasileiro',
+              'XRP-BRL': 'XRP/Real Brasileiro',
+              'ZAR-BRL': 'Rand Sul-Africano/Real Brasileiro'}
 
 
 @bot.message_handler(commands=['acoes'])
@@ -29,9 +62,16 @@ def acoes2(mensagem):
     os.remove(caminho_img)
 
 
-@bot.message_handler(commands=['moedas'])
-def moedas(mensagem):
-    bot.reply_to(mensagem, 'vc selecionou moedas')
+@bot.message_handler(commands=['moedasinst'])
+def moedasinst(mensagem):
+    texto = ''
+    for moeda in moedas_dic:
+        requisicao_dic = requests.get(f'https://economia.awesomeapi.com.br/last/{moeda}').json()[moeda.replace('-', '')]
+        moeda_name = requisicao_dic["name"][:requisicao_dic["name"].find("/")]
+        moeda_code = requisicao_dic["code"]
+        preco_compra = round(float(requisicao_dic["bid"]), 2)
+        texto += f'1 {moeda_code} ({moeda_name}) = {str(preco_compra).replace(".", ",")} {requisicao_dic["codein"]}\n{requisicao_dic["create_date"]}\n\n'
+    bot.reply_to(mensagem, texto)
 
 
 @bot.message_handler(func=lambda mensagem: True)
@@ -39,7 +79,7 @@ def responder(mensagem):
     texto_padrao = """
     Escolha uma opção para continuar (Clique no item):
      /acoes Compara o preço de diferentes ações
-     /moedas Compara o preço de diferentes moedas
+     /moedasinst Compara o preço de diferentes moedas
      Responder qualquer outra coisa não vai funcionar, clique em uma das opções.
     """
     bot.send_message(mensagem.chat.id, texto_padrao)
