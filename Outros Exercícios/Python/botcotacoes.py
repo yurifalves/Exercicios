20795bc615e016ce77b72e6c7a9d29ed13c1b03c
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-03-31 11:54:54 -0300

    ATTBOT

--- Outros Exercícios/Python/botcotacoes.py
+++ Outros Exercícios/Python/botcotacoes.py
@@ -6,30 +6,43 @@ import os
 bot = telebot.TeleBot(TOKEN)
 
 
-'''@bot.message_handler(commands=['opcao1'])
-def opcao1(mensagem):
-    bot.reply_to(mensagem, 'respondendo a opcao1 :)')'''
+@bot.message_handler(commands=['acoes'])
+def acoes1(mensagem):
+    msg_enviada = bot.send_message(mensagem.chat.id, 'Envie o código da ação. Ex: PETR4.SA')
+    bot.register_next_step_handler(msg_enviada, acoes2)
 
 
-def verificar(mensagem):
-    return True
+def acoes2(mensagem):
+    ativo = mensagem.text  # O que o usuário enviou no telegram
+    ativo_df = web.DataReader(ativo, data_source='yahoo', start='2021-03-01', end='2022-03-30')['Close']
+    ativo_df.plot(color='crimson')
+    plt.ylabel('R$', fontsize=15)
+    plt.xlabel('Data', fontsize=15)
+    plt.title(ativo, fontsize=20)
+    plt.legend([ativo])
+    plt.savefig(f'{ativo}.png')
+    caminho_img = os.getcwd() + rf'\{ativo}.png'
+    photo = open(caminho_img, 'rb')
+    bot.send_photo(mensagem.chat.id, photo)
+    bot.send_message(mensagem.chat.id, f'Cotação e Gráfico de "{ativo}" enviados com sucesso!')  # Responde
+    photo.close()
+    os.remove(caminho_img)
 
-@bot.message_handler(func=verificar)
+
+@bot.message_handler(commands=['moedas'])
+def moedas(mensagem):
+    bot.reply_to(mensagem, 'vc selecionou moedas')
+
+
+@bot.message_handler(func=lambda mensagem: True)
 def responder(mensagem):
-    texto = """
+    texto_padrao = """
     Escolha uma opção para continuar (Clique no item):
-     /opcao1 asdfgasdfg
-     /opcao2 asdfgasdfg
-     /opcao3 asdfgasdfg
+     /acoes Compara o preço de diferentes ações
+     /moedas Compara o preço de diferentes moedas
      Responder qualquer outra coisa não vai funcionar, clique em uma das opções.
     """
-    ativo = mensagem.text  # O que o usuário enviou no telegram
-    ativo_df = web.DataReader(ativo, data_source='yahoo', start='2022-03-01', end='2022-03-30')['Close']
-    ativo_df.plot(color='red')
-    plt.savefig(f'grafico-{ativo}.png')
-    caminho_img = os.getcwd() + f'\grafico-{ativo}.png'
-    photo = open(caminho_img, 'rb')
-    bot.send_photo(mensagem.from_user.id, photo)
-    bot.reply_to(mensagem, f'Cotação e Gráfico de "{ativo}" enviados com sucesso!')  # Responde marcando
+    bot.send_message(mensagem.chat.id, texto_padrao)
+
 
 bot.polling()  # Mantém o bot "funcionando/ligado"
