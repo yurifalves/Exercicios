187f61d29627e2cb9440dabee702100559b66e18
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-16 18:49:50 -0300

    Create ex006.c

---
+++ CEV/C/ex006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int num;
+
+    printf("<<< EX006 - ANTECESSOR e SUCESSOR >>>\n\n");
+    printf("Digite um número: ");
+    fflush(stdin);
+    scanf("%i", &num);
+    printf("Analisando o número %i, o antecessor é %i e o sucessor é %i.\n", num, num-1, num+1);
+
+    return 0;
+}
