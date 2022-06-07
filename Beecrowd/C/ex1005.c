0c007e28033e0efadf8b485cd7ab2a5ad63ed034
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-06-06 22:57:38 -0300

    Update ex1005.c

--- Beecrowd/C/ex1005.c
+++ Beecrowd/C/ex1005.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#define media(nota1, nota2) (3.5*nota1+7.5*nota2)/11.0
+
+double media(double nota1, double nota2) {
+    return (3.5*nota1+7.5*nota2)/11.0;
+}
 
 int main() {
     double nota1, nota2;
-    scanf("%f", &nota1);
-    scanf("%f", &nota2);
+    scanf("%lf", &nota1);
+    scanf("%lf", &nota2);
     printf("MEDIA = %.5lf\n", media(nota1, nota2));
 
     return 0;
