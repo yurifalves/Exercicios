15d217ddaa97ffea8aba6e332cfb8967c7790c26
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-07-30 00:49:07 -0300

    Update 1094.c

--- Beecrowd/C/1094.c
+++ Beecrowd/C/1094.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-void saida(int coelhos, int ratos, int sapos);
-
 int main() {
-    int coelhos, ratos, sapos;
-    char stringLida[100];
+    int coelhos = 0, ratos = 0, sapos = 0;
+    char tipo;
     int N;
     scanf("%d", &N);
-    
+
     for (int i=0; i<N; i++) {
-        stringLida = fgets(stringLida, sizeof(stringLida), stdin);
+        int qtd;
+        scanf("%d %c", &qtd, &tipo);
+
+        switch (tipo) {
+            case 'C':
+                coelhos += qtd;
+                break;
+            case 'R':
+                ratos += qtd;
+                break;
+            case 'S':
+                sapos += qtd;
+                break;
+        }
     }
-    
-    saida(coelhos, ratos, sapos);
 
-    return 0;
-}
-
-void saida(int coelhos, int ratos, int sapos) {
     printf("Total: %d cobaias\n", coelhos+ratos+sapos);
-
     printf("Total de coelhos: %d\n", coelhos);
     printf("Total de ratos: %d\n", ratos);
     printf("Total de sapos: %d\n", sapos);
-
     printf("Percentual de coelhos: %.2f %%\n", 100*(float)coelhos/(coelhos+ratos+sapos));
     printf("Percentual de ratos: %.2f %%\n", 100*(float)ratos/(coelhos+ratos+sapos));
     printf("Percentual de sapos: %.2f %%\n", 100*(float)sapos/(coelhos+ratos+sapos));
+
+    return 0;
 }
