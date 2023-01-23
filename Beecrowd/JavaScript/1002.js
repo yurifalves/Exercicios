c95752acf4ebf844660a2149659022ea66c393b7
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-01-23 16:36:26 -0300

    Create 1002.js

---
+++ Beecrowd/JavaScript/1002.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function areaCircunferencia(raio) {
+    const pi = 3.14159;
+    return pi * Math.pow(raio, 2);
+}
+
+const raio = parseFloat(lines.shift());
+console.log(`A=${areaCircunferencia(raio).toFixed(4)}`);
