ca31d61edc395477628b12df2bb38726c3b4a48e
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-02-07 15:04:05 -0300

    Create 1011.js

---
+++ Beecrowd/JavaScript/1011.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const raio = lines.shift();
+
+console.log(`VOLUME = ${volumeEsfera(raio).toFixed(3)}`);
+
+function volumeEsfera(raio) {
+    const pi = 3.14159;
+
+    return 4/3 * pi * Math.pow(raio, 3);
+}
