8428d8ba1762b8fda6f87f4f545d40e7fb7a1c65
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-02-07 16:04:06 -0300

    Create 1012.js

---
+++ Beecrowd/JavaScript/1012.js
@@ -0,0 +1,35 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const valores = lines.shift().split(' ').map(valor => parseFloat(valor));
+
+const a = valores[0];
+const b = valores[1];
+const c = valores[2];
+
+console.log(`TRIANGULO: ${areaTriangulo(a, c).toFixed(3)}`);
+console.log(`CIRCULO: ${areaCirculo(c).toFixed(3)}`);
+console.log(`TRAPEZIO: ${areaTrapezio(a, b, c).toFixed(3)}`);
+console.log(`QUADRADO: ${areaQuadrado(b).toFixed(3)}`);
+console.log(`RETANGULO: ${areaRetangulo(a, b).toFixed(3)}`);
+
+function areaTriangulo(base, altura) {
+    return (base*altura)/2;
+}
+
+function areaCirculo(raio) {
+    const pi = 3.14159;
+    return pi*Math.pow(raio, 2);
+}
+
+function areaTrapezio(base1, base2, altura) {
+    return ((base1+base2)*altura)/2;
+}
+
+function areaQuadrado(lado) {
+    return Math.pow(lado, 2);
+}
+
+function areaRetangulo(base, altura) {
+    return base*altura;
+}
